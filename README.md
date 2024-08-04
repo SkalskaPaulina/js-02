06867b12ff9da0777fefc48d15eb95269ca2c45b
Paulina Skalska <116519135+SkalskaPaulina@users.noreply.github.com>
2024-08-04 12:45:31 +0200

    zadanie zrobione

---
+++ README.md
@@ -0,0 +1,5 @@
+https://github.com/SkalskaPaulina/js-02/
+
+
+Netlify:
+https://fc-js-02.netlify.app
--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-document.querySelector('#convertButton').addEventListener('click', () => {
-	const amount = document.querySelector('#amount').value;
-	const currency = document.querySelector('#currency').value;
-	const loader = document.querySelector('#loader');
-	const result = document.querySelector('#result');
+document.addEventListener("DOMContentLoaded", () => {
+  const amountElement = document.querySelector("#amount");
+  const loaderElement = document.querySelector("#loader");
+  const resultElement = document.querySelector("#result");
 
-	loader.style.display = 'block';
-	result.textContent = '';
+  amountElement.addEventListener("change", () =>
+    amountElement.classList.remove("is-invalid")
+  );
 
-	fetch(`https://api.nbp.pl/api/exchangerates/rates/A/${currency}/?format=json`)
-		.then(response => response.json())
-		.then(data => {
-			const rate = data.rates[0].mid;
-			const convertedAmount = (amount * rate).toFixed(2);
-			result.textContent = `${amount} ${currency} TO ${convertedAmount} PLN`;
-			loader.style.display = 'none';
-		})
-		.catch(() => {
-			result.textContent = 'Wystąpił błąd. Spróbuj ponownie.';
-			loader.style.display = 'none';
-		});
+  amountElement.addEventListener("keydown", () =>
+    amountElement.classList.remove("is-invalid")
+  );
+
+  document
+    .querySelector("#form-currency")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = Object.fromEntries(new FormData(event.target).entries());
+
+      if (!formData.amount || formData.amount <= 0) {
+        amountElement.classList.add("is-invalid");
+        return;
+      }
+
+      loaderElement.style.display = "block";
+      resultElement.textContent = "";
+
+      fetch(
+        `https://api.nbp.pl/api/exchangerates/rates/A/${formData.currency}/?format=json`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const rate = data.rates[0].mid;
+          const convertedAmount = (formData.amount * rate).toFixed(2);
+          resultElement.textContent = `${formData.amount} ${formData.currency} TO ${convertedAmount} PLN`;
+        })
+        .catch(() => {
+          resultElement.textContent = "Wystąpił błąd. Spróbuj ponownie.";
+        })
+        .finally(() => {
+          loaderElement.style.display = "none";
+        });
+    });
 });
